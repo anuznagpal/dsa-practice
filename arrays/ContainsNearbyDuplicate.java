48562f3ee9b52c9813bb273a49a81dcdddd41fed
Anuz Nagpal <anuznagpal@Anuzs-MacBook-Air.local>
2025-12-24 22:43:51 +0530

    Solved  Contains Duplicate II  using HashMap

---
+++ arrays/ContainsNearbyDuplicate.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(!hm.containsKey(nums[i])){
+                hm.put(nums[i],i);
+            }
+            else{
+                if(i-hm.get(nums[i])<=k)
+                return true;
+                hm.put(nums[i],i);
+            }
+
+        }
+        return false;
+    }
+}
