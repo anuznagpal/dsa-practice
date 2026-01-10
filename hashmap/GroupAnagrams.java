80aadf94a2596a7e6d8563a16de5ef192ad1f895
Anuz Nagpal <anuznagpal1808@gmail.com>
2026-01-10 16:01:15 +0530

    Solved  Group Anagrams using HashMap

---
+++ hashmap/GroupAnagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> hm = new HashMap<>();
+        for(String s: strs){
+            char ch[] = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(!hm.containsKey(key)){
+                hm.put(key, new ArrayList<String>());
+            }
+                hm.get(key).add(s);
+        }
+        return new ArrayList<>(hm.values());
+    }
+}
